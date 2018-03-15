8be3e01a284f3f7dc3a0cae81f201887e6cf2719
patty8909 <patty90_30@hotmail.com>
2018-03-15 00:21:32 -0500

    componentes funcionales

---
+++ src/components/product-row.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductRow = ({product}) => {
+    const name = product.stocked ? product.name : <span style={{color:'red'}}>{product.name}</span>
+    return (
+      <tr className="tr-product">
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+      );  
+}
+
+export default ProductRow
---
+++ src/components/product-table.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ProductCategoryRow from './product-category-row'
+import ProductRow from './product-row'
+
+const ProductTable = ({products}) => {
+    const rows = [];
+    let lastCategory = null;
+    //Creando la categoria 
+    products.forEach(product => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow category={product.category} key={product.category} />
+        );
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table className = "table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+}
+
+export default ProductTable
